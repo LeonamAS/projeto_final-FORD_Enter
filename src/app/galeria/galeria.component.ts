b27da6aa971b92241da69f772ccebe6af4bcdb3f
@LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-18 11:56:52 -0300

    Mais imagens na Galeria

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -22,10 +22,17 @@ export class GaleriaComponent {
     { src: '/img/200.jpg', alt: '200 folowers', title: '200' },
     { src: '/img/acorn.jpg', alt: 'Quercus infectoria', title: 'Bolota' },
     { src: '/img/bathroon.jpg', alt: 'Perspectiva', title: 'Pia' },
-    { src: '/img/beach.jpg', alt: 'Lápis de cor', title: 'Pôr-do-Sol' },
+    { src: '/img/beach.jpg', alt: 'Ilha de Maré', title: 'Pôr-do-Sol' },
     { src: '/img/cat.jpg', alt: 'cat', title: 'cat' },
-    { src: '/img/city.jpg', alt: 'city', title: 'city' },
+    { src: '/img/city.jpg', alt: 'Modelo 1983', title: 'Honda City Turbo II' },
     { src: '/img/tv.jpg', alt: 'tv', title: 'tv' },
+    { src: '/img/jimny.jpg', alt: 'Modelo JB23', title: 'Suzuki Jimny' },
+    { src: '/img/joGANdo.jpg', alt: 'Estilo Graffiti', title: 'Canal JoGANdo' },
+    { src: '/img/kitchen.jpg', alt: 'Perspectiva', title: 'Cozinha' },
+    { src: '/img/sunset.jpg', alt: 'Bahia de Todos os Santos', title: 'Pôr-do-Sol' },
+    { src: '/img/wolf.jpg', alt: 'lobo', title: 'Lobo' },
+    { src: '/img/wreckage.jpg', alt: 'Mão Livre', title: 'Destroços' },
+    { src: '/img/zamtrios.jpg', alt: 'Monster Hunter', title: 'Zamtrios' },
   ];
 
   selectedImage: any = null;
