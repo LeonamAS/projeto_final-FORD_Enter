50829cfaafb5f0ba65bafaaccef29768ba3e8563
LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-07-19 17:19:37 -0300

    Fiz a página de contato

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -3,20 +3,22 @@ import { CabecalhoComponent } from '../componentes/cabecalho/cabecalho.component
 import { RodapeComponent } from '../componentes/rodape/rodape.component';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contato',
   imports: [
     CabecalhoComponent,
     RodapeComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent implements OnInit {
   contactForm!: FormGroup;
-
+  
   constructor(
     private fb: FormBuilder,
     private router: Router
@@ -26,26 +28,28 @@ export class ContatoComponent implements OnInit {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: [''], // Opcional, sem validador
+      phone: [''], 
       subject: ['', Validators.required],
       message: ['', [Validators.required, Validators.minLength(10)]],
       consent: [false, Validators.requiredTrue]
     });
   }
 
-   onSubmit(): void {
+  onSubmit(): void {
     if (this.contactForm.valid) {
       console.log('Formulário Válido!');
       console.log(this.contactForm.value);
       alert('Mensagem enviada com sucesso!');
-      this.contactForm.reset(); 
-    } else {
+      this.contactForm.reset();
+      this.contactForm.get('consent')?.setValue(false);
+    } 
+    else {
       console.log('Formulário Inválido');
       this.contactForm.markAllAsTouched();
     }
   }
 
-    onPrivacidade(): void {
+  onPrivacidade(): void {
     this.router.navigate(['/privacidade'])
   }
 }
