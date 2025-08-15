fc7aba1106da528e6b2cb031492d3b15e04080bf
LeonamAS <leonam.sales@ba.estudante.senai.br>
2025-08-14 21:05:00 -0300

    Reorganização da Galeria

--- src/app/galeria/mockAPI.ts
+++ src/app/galeria/mockAPI.ts
@@ -1,23 +1,84 @@
 import { Imagens } from "./imagens.interface";
 
 export const images: Imagens[] = [
-    { src: '/img/200.jpg', alt: 'Lettering - Graffiti', title: '200' },
-    { src: '/img/acorn.jpg', alt: 'Natureza-morta', title: 'Bolota' },
-    { src: '/img/bathroon.jpg', alt: 'Perspectiva', title: 'Pia' },
-    { src: '/img/beach.jpg', alt: 'Lápis de cor', title: 'Pôr-do-Sol' },
-    { src: '/img/cat.jpg', alt: 'Desenho manual', title: 'Gato' },
-    { src: '/img/city.jpg', alt: 'Desenho manual', title: 'Honda City Turbo II' },
-    { src: '/img/tv.jpg', alt: 'Lettering - Graffiti', title: 'TV' },
-    { src: '/img/jimny.jpg', alt: 'Desenho manual', title: 'Suzuki Jimny' },
-    { src: '/img/joGANdo.jpg', alt: 'Lettering - Graffiti', title: 'Canal JoGANdo' },
-    { src: '/img/kitchen.jpg', alt: 'Perspectiva', title: 'Cozinha' },
-    { src: '/img/sunset.jpg', alt: 'Lápis de cor', title: 'Pôr-do-Sol' },
-    { src: '/img/wolf.jpg', alt: 'Caneta Esferográfica', title: 'Lobo' },
-    { src: '/img/wreckage.jpg', alt: 'Mão Livre', title: 'Destroços' },
-    { src: '/img/zamtrios.jpg', alt: 'Lápis de cor', title: 'Zamtrios' },
-    { src: '/img/japan.jpg', alt: 'Lettering - Graffiti', title: 'Japan' },
-    { src: '/img/lightpole.jpg', alt: 'Desenho manual', title: 'Urbano' },
-    { src: '/img/lune.jpg', alt: 'Lettering - Graffiti', title: 'Lune' },
-    { src: '/img/paint.jpg', alt: 'Lettering - Graffiti', title: 'Paint' },
-    { src: '/img/warehouse.jpg', alt: 'Desenho manual', title: 'Depósito' },
-  ];
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreidpejqcz2ivq7xmyxfoj5q4xoi4vub6fpooel374y3bixhet5urgm@jpeg',
+    alt: 'Graffiti',
+    title: 'Paint'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreic5anyicontinx55u2lkeqggycppakzjnxry56i6kjdhdglrzfwve@jpeg',
+    alt: 'Perspective',
+    title: 'Kitchen Sink'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreia2a3u5fqv6mst7676li6ecwzyv47b4f7ncded2kmg226codpufp4@jpeg',
+    alt: 'Perspective',
+    title: 'Warehouse'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreib3casev4brifmonpsuva7iib3afqnduja7jp4iyvjifkhppywk5m@jpeg',
+    alt: '3D Object',
+    title: 'Honda City Turbo II'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreifl7suwrot6ynzc42ca57tmfzakg2uja7jcxa47jqlvby52aayrcq@jpeg',
+    alt: 'Colored Pencils',
+    title: 'Beach'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreibpnjmwss3davogqf6bdpozc7jkwdhycu3njl6vrbz6kweahshozi@jpeg',
+    alt: 'Graffiti',
+    title: '200'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreieuqke6vloxritarjzntl5x3x3b7pskh2cabqawqzyrvb4dyalgly@jpeg',
+    alt: 'Colored Pencils',
+    title: 'Bay'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreibs7lqg6vabxdng4mhicy5clqufpscfkiw3nkbfp5tddn2okquxmy@jpeg',
+    alt: 'Graffiti',
+    title: 'TV'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreiajem6jdsyydszwbqdvfopyrl7sszw3rl33ncnbis4trndapaofky@jpeg',
+    alt: 'Colored Pencils',
+    title: 'Zamtrios'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreicopm3yox5njhnfnrc6xucxqsmk7wclmdbmfh6sxy64nua4dsdsgm@jpeg',
+    alt: 'Still Life',
+    title: 'Acorn'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreibukagaezhc3goddaplkerwvi47ioea34gmtxca6a6uajkk2ga44e@jpeg',
+    alt: 'Perspective',
+    title: 'Bathroom Sink'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreicqt7iex6ssmy45rzsr7yed4cgj22rhgwmwhioe5mkr3sqcwdokda@jpeg',
+    alt: 'Graffiti',
+    title: 'Japan'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreifjq6cvfy77pt66udesgx34jih74qh2oyzguw45ood7nx224of4au@jpeg',
+    alt: '2D Object',
+    title: 'Suzuki Jimny'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreihoyghhsvlwe63het4lrdn7nfok6av3wb3uf42x75glzmgk2n7nle@jpeg',
+    alt: 'Graffiti',
+    title: 'Lune'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreiaupyjhak7neejrawndsvi4i7a7bfo6x76l4vprvo5jl6e3am3phu@jpeg',
+    alt: 'Graffiti',
+    title: 'JoGANdo'
+  },
+  {
+    src: 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:ibd3mkrsgukzs2sdcy5ffben/bafkreihpicgy7273ugtrmaso5y4mak3gfx2amvdiqh3zfqvjazqvsm3mdy@jpeg',
+    alt: 'Perspective',
+    title: 'Urban'
+  }
+];
